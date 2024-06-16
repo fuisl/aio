751d797e6b3306ca43ee04331f338ed0ac3d531d
fuisl <135719107+fuisl@users.noreply.github.com>
2024-06-16 20:51:13 +0700

    refactor: adding SONAR and reformat.

--- module-1/week-2/count_text.py
+++ module-1/week-2/count_text.py
@@ -1,9 +1,20 @@
+"""
+Module: 1
+Week: 2
+HW: count words in text
+"""
+
+
 def count_text(path="P1_data.txt"):
-    with open(path, "r") as file:
+    """
+    Count the number of words in a text file.
+    """
+    with open(path, "r", encoding=str) as file:
         text = file.read()
         text = text.lower()
         text = text.replace("\n", " ")
 
     return {word: text.count(word) for word in text.split()}
 
-print(count_text())
+
+# print(count_text())
--- module-1/week-2/count_word.py
+++ module-1/week-2/count_word.py
@@ -1,6 +1,15 @@
-my_string = 'Happiness'
+"""
+Module: 1
+Week: 2
+HW: count characters in string
+"""
+
 
 def count_char(string):
+    """
+    Count the number of characters in a string.
+    """
     return {char: string.count(char) for char in string}
 
-print(count_char(my_string))
+
+print(count_char("Happiness"))
